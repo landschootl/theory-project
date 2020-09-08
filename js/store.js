2b442f286d2afe744a3468eba0089b3a02a299b6
landchootl <ludovic.landschoot.partner@decathlon.com>
2020-09-08 15:18:11 +0200

    refactor (store): custom html and style

--- js/store.js
+++ js/store.js
@@ -11,12 +11,12 @@ Vue.component('manageStore', {
     }
   },
   template: `
-    <div>
+    <div id="manage-store">
         <h3>Administration du magasin :</h3>
         <button v-if="store.isOpen" @click="closeStore()">Fermer le magasin</button>
         <button v-else @click="openStore()">Ouvrir le magasin</button>
         <p>Nom du magasin : <input v-model.trim="store.name" type="text"/></p>
-        <p>Nom ouvert : <input v-model="store.isOpen" type="checkbox"/></p>
+        <p>Magasin ouvert : <input v-model="store.isOpen" type="checkbox"/></p>
     </div>
   `
 });
@@ -30,10 +30,18 @@ new Vue({
     }
   },
   template: `
-    <div>
-        <h1>{{store.name}}</h1>
-        <h2 v-bind:class="{ 'store-open': store.isOpen, 'store-close': !store.isOpen }">Magasin : {{store.isOpen ? 'open' : 'close'}}</h2>
-        <manage-store :store="store"></manage-store>
+    <div id="store">
+      <div class="bloc">
+        <div class="sub-bloc">
+          <img src="https://img.pngio.com/shop-icon-png-175739-free-icons-library-store-icon-png-1024_1024.jpg" id="img-store"/>
+          <h1>{{store.name}}</h1>
+          <h2 v-bind:class="{ 'store-open': store.isOpen, 'store-close': !store.isOpen }">Magasin : {{store.isOpen ? 'open' : 'close'}}</h2>
+          <manage-store :store="store"></manage-store>
+        </div>  
+        <div class="sub-bloc">
+          
+        </div>
+      </div>  
     </div>
   `
 });
