748a22dcc3cc701f320a7324cc4969a5829eab92
landchootl <ludovic.landschoot.partner@decathlon.com>
2020-09-08 16:28:43 +0200

    feat (store backet): add component

--- js/store.js
+++ js/store.js
@@ -1,16 +1,16 @@
 Vue.component('manageStore', {
-  props: {
-    store: Object
-  },
-  methods: {
-    openStore() {
-      this.store.isOpen = true;
+    props: {
+        store: Object
     },
-    closeStore() {
-      this.store.isOpen = false;
-    }
-  },
-  template: `
+    methods: {
+        openStore() {
+            this.store.isOpen = true;
+        },
+        closeStore() {
+            this.store.isOpen = false;
+        }
+    },
+    template: `
     <div id="manage-store">
         <h3>Administration du magasin :</h3>
         <button v-if="store.isOpen" @click="closeStore()">Fermer le magasin</button>
@@ -22,15 +22,15 @@ Vue.component('manageStore', {
 });
 
 Vue.component('catalogStore', {
-  props: {
-    products: Array
-  },
-  methods: {
-    addProductInBasket(product) {
-      console.log(`Ajout du produit ${product.name} à mon panier`);
-    }
-  },
-  template: `
+    props: {
+        products: Array
+    },
+    methods: {
+        addProductInBasket(product) {
+            this.$emit('addProduct', product);
+        }
+    },
+    template: `
     <div>
         <h3>Les produits en vente :</h3>
         <div v-for="(product, index) in products" :key="index">
@@ -40,21 +40,69 @@ Vue.component('catalogStore', {
   `
 });
 
-new Vue({
-  el:'#app',
-  data: {
-    store: {
-      name: 'Magasin de Fred !',
-      isOpen: false
+Vue.component('basketStore', {
+    props: {
+        products: Array
     },
-    products: [
-      {name: 'Banane', priceHt: 1.50},
-      {name: 'Oeuf', priceHt: 2.20},
-      {name: 'Kiwi', priceHt: 3.30},
-      {name: 'Lait', priceHt: 0.90}
-    ]
-  },
-  template: `
+    computed: {
+        totalPriceTTC() {
+            return this.products.length > 0
+                ? this.products
+                .map(product => product.priceHt * 1.2)
+                .reduce((total, priceTtc) => total + priceTtc)
+                .toFixed(2)
+                : 0;
+        }
+    },
+    data: function () {
+        return {
+            lastUpdateBasket: "no update"
+        }
+    },
+    watch: {
+        products() {
+            const date = new Date();
+            this.lastUpdateBasket = `${date.getHours()}h${date.getMinutes()}m${date.getSeconds()}s`;
+        }
+    },
+    methods: {
+        removeProductInBasket(index) {
+            this.products.splice(index, 1);
+        }
+    },
+    template: `
+    <div>
+        <h3>Mon panier :</h3>
+        <p>Prix total de mon panier TTC : {{totalPriceTTC}} €</p>
+        <p>Dernière modification : {{lastUpdateBasket}}</p>
+        <div v-for="(product, index) in products" :key="index">
+            <span>{{product.name}} - {{product.priceHt}} € <button @click="removeProductInBasket(index)">Retirer de mon panier</button></span>
+        </div>
+    </div>
+  `
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        store: {
+            name: 'Magasin de Fred !',
+            isOpen: false
+        },
+        products: [
+            {name: 'Banane', priceHt: 1.50},
+            {name: 'Oeuf', priceHt: 2.20},
+            {name: 'Kiwi', priceHt: 3.30},
+            {name: 'Lait', priceHt: 0.90}
+        ],
+        basket: []
+    },
+    methods: {
+      addProductInBasket(product) {
+          this.basket.push(product);
+      }
+    },
+    template: `
     <div id="store">
       <div class="bloc">
         <div class="sub-bloc">
@@ -64,7 +112,8 @@ new Vue({
           <manage-store :store="store"></manage-store>
         </div>  
         <div class="sub-bloc">
-          <catalog-store :products="products"></catalog-store>
+          <catalog-store :products="products" @addProduct="addProductInBasket($event)"></catalog-store>
+          <basket-store :products="basket"></basket-store>
         </div>
       </div>  
     </div>
